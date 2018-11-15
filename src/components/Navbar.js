93cc80a2f5210fa380d78ee8ca688a5b0a30d7c6
alyssa0528 <alyssa.kim@gmail.com>
2018-11-15 14:13:00 +0000

    start structuring Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const navbar = (props) => {
+  return (
+    <div>
+      <button>Home</button>
+      <button>Create New Itinerary</button>
+      <button>My Itineraries</button>
+    </div>
+  )
+}
+
+export default navbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Navbar from '../components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1 className="appHeader">Weekend Away</h1>
+        <Navbar />
       </div>
     );
   }
