2f36f68a4df46b1d437c4625e98ce0d3156c8de3
alyssa0528 <alyssa.kim@gmail.com>
2018-11-15 16:06:54 +0000

    add initial state

--- src/containers/Saturday.js
+++ src/containers/Saturday.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Saturday extends Component {
+  state = {
+    saturdayAttractions: []
+  }
+
   render() {
     return (
       <h1>yo!</h1>
--- src/containers/Sunday.js
+++ src/containers/Sunday.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Sunday extends Component {
+  state = {
+    sundayAttractions: []
+  }
+
   render() {
     return (
       <h1>yo!</h1>
