eb9630f1c4b3e56ea2d630a69f68e3f43d0505de
alyssa0528 <alyssa.kim@gmail.com>
2018-11-15 14:59:31 +0000

    create Itineraries, Saturday, and Sunday containers

---
+++ src/containers/Itineraries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Itineraries extends Component {
+  state = {
+    itineraries: []
+  }
+
+  render() {
+    return (
+      <p>test</p>
+    )
+  }
+}
---
+++ src/containers/Saturday.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Saturday extends Component {
+  render() {
+    return (
+      <h1>yo!</h1>
+    )
+  }
+}
